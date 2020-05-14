90325c1eec5cbfc03d117217cc04efeeee2546f7
janguloa <jsuesalnugo@gmail.com>
2020-05-14 11:36:39 -0500

    Consumir API con React

---
+++ src/components/User.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const User = () => {
+
+const {id} = useParams();
+
+const [pueblo, setPueblo] = React.useState([])
+
+    React.useEffect(() => {
+      //  console.log('usando')
+    const obtenerDatos = async () => {
+        const data = await fetch(`https://age-of-empires-2-api.herokuapp.com/api/v1/civilization/${id}`);
+        const users = await data.json();
+        setPueblo(users);
+    }
+        obtenerDatos()
+    },[id])
+
+
+    return (
+        <div>
+            <h3>{pueblo.name}</h3>
+            <p>{pueblo.team_bonus}</p>
+        </div>
+    )
+}
+
+export default User
